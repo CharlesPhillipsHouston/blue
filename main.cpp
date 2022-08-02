9b3eecec111940a7a022a0edc5d4f3f9f1bd71d8
Charles Phillips <51299296+CharlesPhillipsHouston@users.noreply.github.com>
2022-08-02 16:08:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,22 +7,46 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cstring>
+
 #define MAX_PATH = 80  // length of the path is less than 80
 using namespace std;
+
+int satno = 99999; // declare satno, assign initial value
+int year = 20; // define year
+string int_des = "01A"; // define international designator
+
+string line;
 
 int main(int argc, const char * argv[])
 
 {
-    char iod_input;
-    // iod_input is the test file here
    
-    cout << "enter file name";
+    string ifname("/Users/Charles/Desktop/common_files/iod_input.txt");
+    string ofname("/Users/Charles/Desktop/common_files/iod_output.txt");
     
-    cin.getline(iod_input, MAX_PATH);
+    ifstream fin(ifname);
     
-    ofstream file_out(iod_input);
+    ofstream fout(ofname);
     
+    int i = 0; // set up the way to count in the while loop
+    
+    while (i < 4)
+    {
+        string line;  //  create line
+        getline(fin, line);
+      
+        ++i;  // increment i to stop while loop
+    
+    const char *s = line.c_str();
+    sscanf(s, "%d" "%d" "%s", &satno, &year, &int_des);
+    
+    fout << "satno " << satno <<  " year " << year << " int_des " << int_des << endl; // output
+   
+    }  // end of while loop
+
     return 0;
-}
+} // end of main
 
 //   Users/Charles/Desktop/common_files/iod_input.txt
